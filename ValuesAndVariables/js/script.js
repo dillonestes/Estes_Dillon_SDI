6d9c213cf9d9b0e138363f4b539e69ecb9ec5d48
dillonestes <Dillon1@Dillons-MacBook-Pro.local>
2015-05-14 16:49:53 -0600

    Added Values and Variables
    Used different types of values for variables

---
+++ ValuesAndVariables/js/script.js
@@ -0,0 +1,26 @@
+
+var yearBorn = 1993;
+
+console.log(yearBorn)
+
+// alert("Hello world!");
+
+var age; // declaration 
+
+age = 22; // definition
+
+var yearOfBirth = 1993; // declaration and definition
+
+// Different types of variables
+
+var isStudent = true;
+
+var firstName = "Dillon";
+
+var hasLineBreak = 'These aren\'t the droids you\'re looking for. \n Move along';
+
+// Arrays and accessing data
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo", firstName];
+
+console.log(muppetNames[4] + "is not a muppet.");
