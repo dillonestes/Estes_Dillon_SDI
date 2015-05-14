4a5fbc791090b2ced44c8a5d5c9dbdab24976d98
dillonestes <Dillon1@Dillons-MacBook-Pro.local>
2015-05-14 17:29:05 -0600

    Using Expressions
    using expressions to calculate

---
+++ Expressions/js/script.js
@@ -0,0 +1,93 @@
+
+// Assignment operator
+
+var a = 2;
+
+//a = a + 3;
+
+var b = a + 3; 
+
+console.log(b);
+
+var yearBorn = 1993;
+
+var age = 2015 - yearBorn;
+
+console.log(age);
+
+// area of triangle
+
+var width = 10;
+var height = 30;
+
+var area = (width * height)/2;
+
+console.log(area);
+
+// Using modulo operator
+
+var remainder = 20 % 12;
+
+console.log(remainder);
+
+// order of operations
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4
+
+console.log("The average test score is " + average);
+
+var length = 7;
+var width = 8;
+
+var perimiter = length * 2 + width * 2;
+
+console.log("The perimeter is " + perimiter);
+
+// Arrays and expressions
+
+var oranges = [200, 342, 179];
+
+var total = oranges[0] + oranges[2] + oranges[3];
+
+console.log(total);
+
+// Concatenting strings
+
+var firstName = "Dillon";
+var lastName = "Estes";
+
+var name = firstName + " " + lastName;
+
+console.log("My name is " + name);
+
+// Casting 
+
+var stringVar = "6";
+
+var result = 7 + Number(stringVar);
+
+console.log(result);
+
+// numbers to string
+
+var areaCode = 719;
+var p1 = 641;
+var p2 = 6029;
+
+var phoneNum = "(" + String(areaCode) + ")" + String(p1) + "-" + String(p2);
+
+console.log(phoneNum);
+
+// Assignment operators
+
+var a = 3;
+
+// a += 4;
+// a ++
+a --; // = 2
+console.log(a);
