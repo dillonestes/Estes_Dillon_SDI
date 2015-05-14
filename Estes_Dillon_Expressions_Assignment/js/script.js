cc147d297c11aa4052ef1ebe1926f92dcc58c3c6
dillonestes <Dillon1@Dillons-MacBook-Pro.local>
2015-05-14 17:47:04 -0600

    Expressions Assignment
    added prompt variables

--- Estes_Dillon_Expressions_Assignment/js/script.js
+++ Estes_Dillon_Expressions_Assignment/js/script.js
@@ -3,4 +3,4 @@
 var docWidth = prompt("Let's center our content in our webpage. Let's start with the width of the document:");
 var docHeight = prompt("Next let's calculate the height of the document:");
 var containerWidth = prompt("Great, so what'ss the width of the content we want to center?");
-var containerHeight = prompt("Alright, now whats the height of the content we want to center?");
+var containerHeight = prompt("Alright, now whats the height of the content we want to center?");  
