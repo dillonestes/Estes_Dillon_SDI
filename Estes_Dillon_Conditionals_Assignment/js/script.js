2772a42b1a1fb2ba92dcd5c003a7a14d56cbc01b
dillonestes <Dillon1@Dillons-MacBook-Pro.local>
2015-05-21 16:32:07 -0600

    Conditionals Assignment variables
    created variables for user input.

---
+++ Estes_Dillon_Conditionals_Assignment/js/script.js
@@ -0,0 +1,7 @@
+// Dillon Estes 05-21-2015 Conditionals Assignment
+
+// Let's make a love calculator!
+var firstName = prompt("What's your first name?");// Get first name
+var lastName = prompt("What's your last name?"); // Get last name
+var soFirstName = prompt("What's your significant other's first name?"); // Get other person's first name
+var soLastName = prompt("What's your significant other's last name?"); // Get other person's last name
