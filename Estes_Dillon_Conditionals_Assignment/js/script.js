082ee5393ff0665b5ec3de2611dc4f9d74ab0404
dillonestes <Dillon1@Dillons-MacBook-Pro.local>
2015-05-21 16:46:00 -0600

    Conditonals Assignment
    added first conditional

--- Estes_Dillon_Conditionals_Assignment/js/script.js
+++ Estes_Dillon_Conditionals_Assignment/js/script.js
@@ -5,3 +5,16 @@ var firstName = prompt("What's your first name?");// Get first name
 var lastName = prompt("What's your last name?"); // Get last name
 var soFirstName = prompt("What's your significant other's first name?"); // Get other person's first name
 var soLastName = prompt("What's your significant other's last name?"); // Get other person's last name
+
+var isMatch = 20;
+
+var first = firstName.length; // Get string length
+var second = lastName.length; // Get string length
+var soFirst = soFirstName.length; // Get string length
+var soSecond = soLastName.length; // Get string length
+
+if(first && second == first && second) { // Check to see if the length of first and last names are the same
+	var firstValue = (first * second) + isMatch; // Add the str length of first and second, then subtract isMatch
+} else {
+	firstValue =	 (first * second) - 5;
+}
